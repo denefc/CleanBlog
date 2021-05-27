ad3e20920cce2040bc7f537a94427b7a92df14af
denefc <mehmet_7kezman@hotmail.com>
2021-05-27 16:23:05 +0300

    ejs configure

--- app.js
+++ app.js
@@ -1,25 +1,49 @@
 const express = require("express");
+const mongoose=require('mongoose')
+
 const ejs=require("ejs");
+const path = require("path");
+const Post=require('./models/Post')
+
 const app = express();
+
+//connect DB
+mongoose.connect('mongodb://localhost/cleanblog-test-db',{
+  useNewUrlParser:true,
+  useUnifiedTopology:true,
+});
+
 
 //templates engine
 app.set("view engine", "ejs");
 
+//middlewares
 app.use(express.static("public"));
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
 
-app.get("/", (req, res) => {
-  res.render("index");
+//Routes
+app.get("/", async (req, res) => {
+  const posts=await Post.find({})
+  res.render("index",{
+    posts,
+  });
 });
 app.get("/about", (req, res) => {
   res.render("about");
 });
+app.post("/posts", (req, res) => {
+  Post.create(req.body)
+  res.redirect("/")
+});
 app.get("/add_post", (req, res) => {
   res.render("add_post");
 });
-app.get("/post", (req, res) => {
-  res.render("post");
-});
+
+
+
+
 
 const port = 3000;
 app.listen(port, () => {
